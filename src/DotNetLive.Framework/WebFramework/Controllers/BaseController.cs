7e8c7e280f9e11521d5e67e4f47579f03d48596d
Duke Cheng <dk@feinian.me>
2017-02-26 22:06:31 +0800

    added portal security part

---
+++ src/DotNetLive.Framework/WebFramework/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using DotNetLive.Framework.Models;
+using System;
+
+namespace DotNetLive.Framework.WebFramework.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        public string Token
+        {
+            get
+            {
+                if(!User.Identity.IsAuthenticated)
+                {
+                    throw new Exception("你没有登陆");
+                }
+                return User.Claims.SingleOrDefault(x => x.Type == ApplicationUser.JwtClaimName)?.Value;
+            }
+        }
+    }
+}
